14a7a62789c18254f953d652036367e17155a6a3
Javehdez <javehdez@gmail.com>
2013-11-29 09:31:52 -0600

    merged rozgo

--- Assets/Blocks/Unit.cs
+++ Assets/Blocks/Unit.cs
@@ -1,19 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-namespace Blocks {
-
-    public class Unit : Block {
-
-        public Vector3 Position;
-
-        public override void OnStart () {
-            base.OnStart();
-        }
-
-        public override void OnUpdate () {
-            base.OnUpdate();
-        }
-    }
+namespace Blocks
+{
+  public class Unit : Block
+  {
+    public Vector3 Position;
+  }
 }
 
