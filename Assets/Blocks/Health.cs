2fa3a243673fb25b18b741c864234ed25dd4d7c6
Javehdez <javehdez@gmail.com>
2013-11-29 11:44:07 -0600

    Pulling

--- Assets/Blocks/Health.cs
+++ Assets/Blocks/Health.cs
@@ -4,5 +4,12 @@ using System.Collections;
 namespace Blocks {
 
     public class Health : Block {
+
+        public void OnAttacked(int DPS)
+        {
+        }
+        public void OnAttackedEnd(int DPS)
+        {
+        }
     }
 }
